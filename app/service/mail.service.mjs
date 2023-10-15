29fbce01fc1d29d7a024b733a6a6796a151b9d57
orajiakuchibuchi <somtobuchi@gmail.com>
2023-10-15 13:11:27 +0100

    merging with mail server

--- app/service/mail.service.mjs
+++ app/service/mail.service.mjs
@@ -2,12 +2,21 @@ import needle from "needle";
 import dotenv from "dotenv";
 // import http from "http";
 import request from "request";
+import * as nodemailer from "nodemailer";
 dotenv.config();
 // const request = require('request');
 
 const MAILER_API = process.env.MAIL_SERVER_URL;
 
 export class MailService{
+    transporter = nodemailer.createTransport({
+        host: "notify@zinder.com.ng",
+        port: 465,
+        auth: {
+          user: "subscribers@hireus.org.ng", //replace with your email
+          pass: "Ea6qH9{+_E3S ", //replace with your password
+        },
+      });
     constructor(){}
     single(subject, from, to, html, cb, cc=[], bcc=[]){
         let data = {from,to,subject,cc,html,bcc};
@@ -35,6 +44,24 @@ export class MailService{
         //     console.error(err) ;
         //     console.error("err") ;
         // })
+    }
+    send(mailOptions={
+        from:  '', //replace with your email
+        to: '',
+        subject: '',
+        bcc: '',
+        html:''}){
+            return new Promise((resolve, reject) => {
+                return this.transporter.sendMail(mailOptions, (err, result) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    console.log("Message sent: %s", result.messageId);
+                    resolve(result);
+               });
+              });
+
     }
 
 }
